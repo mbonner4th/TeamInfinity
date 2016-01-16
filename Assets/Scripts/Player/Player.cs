7f0bd021c59d7c9e3c28e21c220ced6518fcf0b8
Miles <mbonner4th@gmail.com>
2016-01-16 09:23:33 -0800

    Fixing Merge? Maybe?

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Character
+{
+    public int water;
+    public int ammo;
+
+	public GameObject projectile;
+	public Ray2D mousePos;
+
+	public override void OnCollision(Character other)
+    {
+        print("Player colliding!");
+        GameObject.Destroy(other.gameObject);
+    }
+
+	public override void BaseUpdate(float dt)
+	{
+		base.BaseUpdate (dt);
+
+		// Fire a bullet in the direction of the mouse when you click!
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 pos = Input.mousePosition;
+			pos.z = transform.position.z - Camera.main.transform.position.z;
+			pos = Camera.main.ScreenToWorldPoint(pos);
+			Quaternion q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
+
+			GameObject go = (GameObject)Instantiate(projectile, transform.position, q);
+			Rigidbody2D bulletRb = go.GetComponent<Rigidbody2D>();
+			bulletRb.AddForce(go.transform.up * 1000.0f);
+		}
+	}
+
+    public override void OnPickup(Pickup other)
+    {
+        if (other.type == 1)
+        {
+            level.OnPickPart(other.intensity);
+            print("Picked up a part!");
+        }
+        else if (other.type == 2)
+        {
+            health += other.intensity;
+            print("Picked up health!");
+        }
+        else if (other.type == 3)
+        {
+            water += other.intensity;
+            print("Picked up water!");
+        }
+        else if (other.type == 4)
+        {
+            ammo += other.intensity;
+            print("Picked up ammo!");
+        }
+        else if (other.type == 5)
+        {
+            level.OnPickPerson(other.intensity);
+            print("Saved a person!");
+        }
+
+        GameObject.Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : Base
+{
+	public int team;
+	public int countdown;
+	
+	public override void BaseStart() 
+	{
+		countdown = 50;
+	}
+	
+	public override void BaseUpdate(float dt)
+	{
+		if (countdown-- <= 0) {
+			GameObject.Destroy (gameObject);
+		}
+	}
+	
+	public virtual void OnTriggerEnter2D(Collider2D other)
+	{
+		Character otherCharacter = other.GetComponent<Character>();
+		if (otherCharacter == null) {
+			return;
+		}
+		
+		//print("Collide!!");
+		if (otherCharacter.team != team) {
+			OnCollision(otherCharacter);
+		}
+	}
+	
+	public virtual void OnCollision(Character other)
+	{
+		print("Player colliding!");
+		GameObject.Destroy(other.gameObject);
+		GameObject.Destroy (gameObject);
+	}
+}
