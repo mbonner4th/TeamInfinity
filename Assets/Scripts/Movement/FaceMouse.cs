09e072a56daa4f50d121d4160e8411bfb8a39ba8
Ben <bslupik@uci.edu>
2016-01-16 09:45:40 -0800

    Missed some stuff... oops

---
+++ Assets/Scripts/Movement/FaceMouse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Input.mousePosition;
+		pos.z = transform.position.z - Camera.main.transform.position.z;
+		pos = Camera.main.ScreenToWorldPoint(pos);
+		transform.rotation = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
+	}
+}
